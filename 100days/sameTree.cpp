379cdf26b76a832e4905aeb555faac9d0032017d
Deniz Ece Aktaş <aktasdeak@gmail.com>
2022-03-01 19:49:51 +0300

    Add files via upload

---
+++ 100days/sameTree.cpp
@@ -0,0 +1,24 @@
+//leetcode 100
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(q == NULL && p ==NULL){
+            return true;
+        }
+        if(q != NULL && p ==NULL){
+            return false;
+        }
+        if(q == NULL && p !=NULL){
+            return false;
+        }
+        if(q != NULL && p !=NULL){
+            return (q->val==p->val) &&isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        }
+        
+        
+        
+        
+        return false;
+    }
+};
